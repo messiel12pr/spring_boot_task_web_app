a9a64c895ecd63efd60174630de7e696833edada
= <=>
2023-06-04 15:17:43 -0400

    Coded logic for extracting a user from the database in CustomUserDetailsService

--- src/main/java/com/joelm/repository/UserRepository.java
+++ src/main/java/com/joelm/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.joelm.repository;
 
 import com.joelm.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User findByUsername(String username);
 }
--- src/main/java/com/joelm/service/CustomUserDetailsService.java
+++ src/main/java/com/joelm/service/CustomUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.joelm.service;
 
+import com.joelm.entity.User;
+import com.joelm.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+
+        if(user == null)  {
+            throw new UsernameNotFoundException("User: " + username + " not found!");
+        }
+
         return null;
     }
 }

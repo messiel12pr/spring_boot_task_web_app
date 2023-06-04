3d9e83b91932eadb96f023c469b8bf76ab8b5319
= <=>
2023-06-04 17:00:17 -0400

    Added configuration for authentication

--- src/main/java/com/joelm/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/joelm/configuration/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,4 +25,15 @@ public class WebSecurityConfiguration {
         provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
     }
+
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/")
+                        .permitAll()
+                        .requestMatchers("/home")
+                        .authenticated()
+                );
+    }
+
 }

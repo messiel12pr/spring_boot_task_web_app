44e3cecc3fec11fca34fb0984caf33fd0af616e7
Joel M. Gonzalez <joelm12pr@gmail.com>
2023-06-05 16:55:13 -0400

    Created command line runner for role assignment at the start of the program

--- src/main/java/com/joelm/TaskWebAppApplication.java
+++ src/main/java/com/joelm/TaskWebAppApplication.java
@@ -1,13 +1,37 @@
 package com.joelm;
 
+import com.joelm.model.ApplicationUser;
+import com.joelm.model.Role;
+import com.joelm.repository.RoleRepository;
+import com.joelm.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class TaskWebAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskWebAppApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			ApplicationUser admin = new ApplicationUser(1,"admin", passwordEncoder.encode("password"), roles);
+
+		};
+	}
 }
